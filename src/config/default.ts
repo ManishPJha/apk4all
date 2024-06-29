4dcb83dc506057211dd64d6708966b2b7988ffa9
manish <mjha205@rku.ac.in>
2024-06-29 20:39:09 +0530

    env changes

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,6 +1,7 @@
 const config = {
   appName: "apk4all",
-  appUrl: process.env.NEXT_PUBLIC_BASE_URL as string,
+  appUrl:
+    (process.env.NEXT_PUBLIC_VERCEL_URL as string) || "http://localhost:3000",
   blogsFolder: "src/content/posts",
   authorsFolder: "src/content/authors",
   defaultReadTime: 3,
